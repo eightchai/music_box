d145b0af32d579ffaae53ee682d38e9c85465d3f
SamsUsername <sjs357@scarletmail.rutgers.edu>
2015-03-05 22:10:50 -0500

    changed colors of buttons when hovered and clicked

--- javascript/home.js
+++ javascript/home.js
@@ -12,7 +12,8 @@ for (var hor = 0; hor < 24; hor++){ // vertical diff notes
             top: 28*vert+1,
             fill: '#E6E6E6',
             width: 23,
-            height: 23
+            height: 23,
+            selected: 0
             });
             
         rect.hasControls = rect.hasBorders = false;
@@ -25,12 +26,43 @@ for (var hor = 0; hor < 24; hor++){ // vertical diff notes
 }
 
 canvas.on({
-    'mouse:down': function(e){
+    'mouse:down': function(e){ //color when you click on a square
         if (e.target){
-            e.target.fill = '#666666';
-            canvas.RenderAll();
+            if (e.target.selected == 0){
+                e.target.fill = '#666666';
+                e.target.selected = 1;
+                canvas.renderAll();
+            }else{
+                e.target.fill = '#E6E6E6';
+                e.target.selected = 0;
+                canvas.renderAll();
+            }
+        }
+    },
+    'mouse:over': function(e){ //color when you hover over a square
+        if(e.target){
+            if (e.target.selected == 0){
+                e.target.setFill('#DAFFB5');
+                canvas.renderAll();
+            }
+            else{
+                e.target.setFill('#007A00');
+                canvas.renderAll();
+            }
+        }
+    },
+    'mouse:out': function(e){ //color when you're done hovering over a square
+        if(e.target){
+            if (e.target.selected == 0){
+                e.target.setFill('#E6E6E6');
+                canvas.renderAll();
+            }else{
+                e.target.setFill('#666666');
+                canvas.renderAll();
+            }
         }
     }
 });
 this.__canvases.push(canvas);
+
 
