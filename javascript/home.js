2110f8ea0f2858a2f31242b04165915a74419a39
SamsUsername <sjs357@scarletmail.rutgers.edu>
2015-03-16 21:38:18 -0400

    added all notes in key of C major

--- javascript/home.js
+++ javascript/home.js
@@ -17,6 +17,75 @@ var defaultColor = '#4C4C4C';
 var hoverColor = '#AEAEAE';
 var selectedColor = ['#E825C6','#9144FF','#5AADF5','#4FF593','#FFF343','#FF923C','#E80D06'];
 
+var sound1c = new Howl({
+    urls: ['./mp3s/note1-c.mp3']
+});
+var sound2c = new Howl ({
+    urls: ['./mp3s/note2-c.mp3']
+});
+var sound3c = new Howl ({
+    urls: ['./mp3s/note3-c.mp3']
+});
+var sound4c = new Howl ({
+    urls: ['./mp3s/note4-c.mp3']
+});
+var sound5c = new Howl ({
+    urls: ['./mp3s/note5-c.mp3']
+});
+var sound6c = new Howl ({
+    urls: ['./mp3s/note6-c.mp3']
+});
+var sound7c = new Howl ({
+    urls: ['./mp3s/note7-c.mp3']
+});
+var sound8c = new Howl ({
+    urls: ['./mp3s/note8-c.mp3']
+});
+var sound9c = new Howl ({
+    urls: ['./mp3s/note9-c.mp3']
+});
+var sound10c = new Howl ({
+    urls: ['./mp3s/note10-c.mp3']
+});
+var sound11c = new Howl ({
+    urls: ['./mp3s/note11-c.mp3']
+});
+var sound12c = new Howl ({
+    urls: ['./mp3s/note12-c.mp3']
+});
+var sound13c = new Howl ({
+    urls: ['./mp3s/note13-c.mp3']
+});
+var sound14c = new Howl ({
+    urls: ['./mp3s/note14-c.mp3']
+});
+var sound15c = new Howl ({
+    urls: ['./mp3s/note15-c.mp3']
+});
+var sound16c = new Howl ({
+    urls: ['./mp3s/note16-c.mp3']
+});
+var sound17c = new Howl ({
+    urls: ['./mp3s/note17-c.mp3']
+});
+var sound18c = new Howl ({
+    urls: ['./mp3s/note18-c.mp3']
+});
+var sound19c = new Howl ({
+    urls: ['./mp3s/note19-c.mp3']
+});
+var sound20c = new Howl ({
+    urls: ['./mp3s/note20-c.mp3']
+});
+var sound21c = new Howl ({
+    urls: ['./mp3s/note21-c.mp3']
+});
+var sound22c = new Howl ({
+    urls: ['./mp3s/note22-c.mp3']
+});
+
+var soundArray = [sound22c, sound21c, sound20c, sound19c, sound18c, sound17c, sound16c, sound15c, sound14c, sound13c, sound12c, sound11c, sound10c, sound9c, sound8c, sound7c, sound6c, sound5c, sound4c, sound3c, sound2c, sound1c];
+
 //makes the grid
 var grid = [];
 for (var hor = 0; hor < 24; hor++){ // vertical diff notes  
@@ -103,7 +172,7 @@ canvas.on({
 //this.__canvases.push(canvas);
 
 var i = 0; 
-function timeout(){ //makes selected blocks glow on a timer
+function timeout(){ //makes selected blocks glow on a timer & makes them play a note
     setTimeout(function (){
         if (i > grid.length-1){
             i = 0;
@@ -111,6 +180,7 @@ function timeout(){ //makes selected blocks glow on a timer
         for(var j = 0; j<grid[i].length; j++){
             if (grid[i][j].selected == 1){
                 grid[i][j].opacity = 1;
+                soundArray[j].stop().play();
             }
         }
         i++;        
